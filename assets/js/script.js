6e926e683fba56a27908e7593448bc5859aacbc8
Luke Thornton <725682@student.cityofbristol.ac.uk>
2024-04-20 21:37:09 +0100

    feat: added multiply

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}.Aborting!`;
@@ -42,10 +44,12 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert("Hey! You got it right! :D");
+    incrementScore();
   } else {
     alert(
       `Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`
     );
+    incrementWrongAnswer();
   }
   runGame(calculatedAnswer[1]);
 }
@@ -61,15 +65,29 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
     return [operand1 + operand2, "addition"];
+  } else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
   } else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}, Aborting!`;
   }
 }
 
-function incrementScore() {}
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+}
 
-function incrementWrongAnswer() {}
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
@@ -79,4 +97,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
+}
